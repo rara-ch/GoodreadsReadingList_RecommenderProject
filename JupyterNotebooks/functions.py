eec48d144079165c968aa63ce6e08ffe8f2133f0
rara-ch <rahulchitkara2001@gmail.com>
2025-07-21 14:18:58 +1000

    Reviews text preprocessing, text preprocessing helper function update, minor feature engineering (days_to_finish columns)

--- JupyterNotebooks/functions.py
+++ JupyterNotebooks/functions.py
@@ -1,26 +1,32 @@
 # Import necessary libraries
 import pandas as pd
 import spacy
+from tqdm import tqdm
+
+# Adds progress bar to pd.DataFrame.apply method
+tqdm.pandas()
 
 # Download the spacy model
-nlp = spacy.load("en_core_web_sm")
+nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
 
 # Helper functions for text preprocessing (data cleaning) section
 def lower_replace(series):
-    output = series.str.lower()
-    output = output.str.replace(r'\[.*?\]', '', regex=True)
-    output = output.str.replace(r'[^\w\s]', '', regex=True)
+    output = series.str.lower()                                # Turn all uppercase characters to lowercase
+    output = output.str.replace(r'[^\w\s]', '', regex=True)    # Remove all punctution
+    output = output.str.replace(r'\s*\n', '', regex=True)      # Remove '\n' and leading whitespace
     return output
 
-def token_lemma_nonstop(text):
-    doc = nlp(text)
-    output = [token.lemma_ for token in doc if not token.is_stop]
-    return ' '.join(output)
-
-def clean_and_normalize(series):
-    output = lower_replace(series)
-    output = output.apply(token_lemma_nonstop)
-    return output
+def clean_and_normalise(series):
+    cleaned = lower_replace(series).fillna('')                 # lowercase, remove punctuation, and remove NaN values
+    processed_texts = nlp.pipe(cleaned, batch_size=1000)       # process texts in batches with nlp.pipe()
+    
+    # lemma and remove stopwords for each doc
+    output = []
+    for doc in tqdm(processed_texts, total=len(series)):
+        tokens = [token.lemma_ for token in doc if not token.is_stop]
+        output.append(' '.join(tokens))
+    
+    return pd.Series(output)
 
 # Helper function for EDA in the recommender section
 def get_percentiles(df, column):
@@ -40,4 +46,4 @@ def get_percentiles(df, column):
 
 # Allow command-line execution
 if __name__ == "__main__":
-    print("Text preprocessing module ready to use.")
+    print("functions module ready to use.")
